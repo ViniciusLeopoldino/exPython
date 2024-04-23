cdcbea08c8a79346d4a470aba1b5f5516649c355
ViniciusLeopoldino <vi.95@hotmail.com>
2024-04-23 00:07:19 -0300

    v3

---
+++ FIAP/-CHALLENGE/Challenge-main/Challenge-main/FRONT-END DESIGN ENGINEERING/meuPorto/javaScript/popup.js
@@ -0,0 +1,5 @@
+  const registerButton = document.getElementById("registerButton");
+  registerButton.addEventListener("click", function() {
+      // Redirecionar para a página de registro
+      window.location.href = "menu.html";
+  });

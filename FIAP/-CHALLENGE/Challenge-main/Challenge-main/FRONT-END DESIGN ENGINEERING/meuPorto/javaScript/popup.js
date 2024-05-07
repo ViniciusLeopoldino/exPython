da422272b3003351cca1162d567ec3e94a85b89d
Vinicius Leopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2024-05-06 23:20:05 -0300

    Delete FIAP directory

--- FIAP/-CHALLENGE/Challenge-main/Challenge-main/FRONT-END DESIGN ENGINEERING/meuPorto/javaScript/popup.js
+++
@@ -1,5 +0,0 @@
-  const registerButton = document.getElementById("registerButton");
-  registerButton.addEventListener("click", function() {
-      // Redirecionar para a página de registro
-      window.location.href = "menu.html";
-  });
